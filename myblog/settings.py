d77271fa21bb6c0616504399118eee389c9d3f08
joelproxi <joeledmond95@gmail.com>
2021-10-22 02:09:30 +0200

    video 23 et 24

--- myblog/settings.py
+++ myblog/settings.py
@@ -69,12 +69,25 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'tuto',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
+
 
 
 # Password validation
